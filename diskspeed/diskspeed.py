145fadb251c6fed9804e9eeb9dab464820d92301
Anstedt <anstedt@gmail.com>
2024-02-27 18:27:17 -0600

    Convert to proper class

--- diskspeed/diskspeed.py
+++ diskspeed/diskspeed.py
@@ -3,81 +3,100 @@ import os
 from tkinter import *
 from tkinter import ttk
 
-print("Start Disk Letters")
-drives_cmd="wmic logicaldisk get name > logd.log"
-os.system(drives_cmd)
+class DiskSpeed:
+  def __init__(self):
+    self.root = Tk()
+    self.get_disks()
+    self.get_disk_info()
+    self.parse_disk_info()
+    self.display_disk_info()
+    
+  def get_disks(self):
+    print("Start Disk Letters")
+    drives_cmd = "wmic logicaldisk get name > drive_letters.log"
+    os.system(drives_cmd)
 
-# Now read the file and process it
-file = open("logd.log", 'rt')
-lines = file.read().splitlines()
-file.close()
+    # Now read the file and process it
+    file = open("drive_letters.log", 'rt')
+    lines = file.read().splitlines()
+    file.close()
 
-for line in lines:
-  index = line.find(":")
-  if (index != -1):
-    print(line[0:2])
+    for line in lines:
+      index = line.find(":")
+      if (index != -1):
+        print(line[0:2])
 
-print("Start Disk Speed")
-# Disk Spee
-dsk_drv = "C"
+  def get_disk_info(self):
+    print("Start Disk Speed")
+    # Disk Speed
+    dsk_drv = "C"
 
-logf="hja"+dsk_drv+".log"
+    logf="hja"+dsk_drv+".log"
 
-# This gets the disk speed aand saves it to a file
-cmd = "winsat disk -drive C > " + logf
-# os.system(cmd)
+    # This gets the disk speed aand saves it to a file
+    cmd = "winsat disk -drive C > " + logf
+    # os.system(cmd)
 
-# Now read the file and process it
-file = open(logf, 'rt')
+    # Now read the file and process it
+    file = open(logf, 'rt')
 
-lines = file.read().splitlines()
-file.close()
+    self.lines = file.read().splitlines()
+    file.close()
 
-list_index = 0
-title_list = list(("A", "B", "C"))
-value_list = list(("A", "B", "C"))
-units_list = list(("A", "B", "C"))
+  def parse_disk_info(self):
+    list_index = 0
+    self.title_list = list(("A", "B", "C"))
+    self.value_list = list(("A", "B", "C"))
+    self.units_list = list(("A", "B", "C"))
 
-for line in lines:
-  index = line.find("Disk")
-  if (index != -1):
-    # Find the index to the end of the title by looking for trailing spaces
-    title_index = line.find("   ")
-    # The result is from the end of the title to the end of the string, and get rid of leading and trailing spaces
-    result = line[title_index:].strip()
-    # Now find the end of the result so we can get the value and units but not the trailing numbers
-    result_index = result.find("   ")
-    # This is the result and the units
-    result = result[:result_index]
+    for line in self.lines:
+      index = line.find("Disk")
+      if (index != -1):
+        # Find the index to the end of the title by looking for trailing spaces
+        title_index = line.find("   ")
+        # The result is from the end of the title to the end of the string, and get rid of leading and trailing spaces
+        result = line[title_index:].strip()
+        # Now find the end of the result so we can get the value and units but not the trailing numbers
+        result_index = result.find("   ")
+        # This is the result and the units
+        result = result[:result_index]
 
-    # Now pick up the pieces we want
-    title_list[list_index] = line[index:title_index]
-    value_list[list_index] = float(result[:result.find(" ")])
-    units_list[list_index] = result[result.find(" "):]
-    list_index = list_index + 1
-    # print("title=", title, "result=", result, "value=", value, "units=", units)
-    # print(len(title_list))
-    # print(title_list)
+        # Now pick up the pieces we want
+        self.title_list[list_index] = line[index:title_index]
+        self.value_list[list_index] = float(result[:result.find(" ")])
+        self.units_list[list_index] = result[result.find(" "):]
+        list_index = list_index + 1
+        # print("title=", title, "result=", result, "value=", value, "units=", units)
+        # print(len(title_list))
+        # print(title_list)
 
-root = Tk()
-ttk.Entry(root).grid()
+  def display_disk_info(self):
+    ttk.Entry(self.root).grid()
 
-lp = ttk.Label(root, text="Title", font="helvetica 24", width=19)
-lp.grid(padx=0, pady=0)
+    lp = ttk.Label(self.root, text="Title", font="helvetica 24", width=19)
+    lp.grid(padx=0, pady=0)
 
-l1 = ttk.Label(root, text=title_list[0], font="helvetica 16", width=28)
-l1.grid(padx=1, pady=0)
+    l1 = ttk.Label(self.root, text=self.title_list[0], font="helvetica 16", width=28)
+    l1.grid(padx=1, pady=0)
 
-l2 = ttk.Label(root, text=title_list[1], font="helvetica 16", width=28)
-l2.grid(padx=1, pady=0)
+    l2 = ttk.Label(self.root, text=self.title_list[1], font="helvetica 16", width=28)
+    l2.grid(padx=1, pady=0)
 
-l3 = ttk.Label(root, text=title_list[2], font="helvetica 16", width=28)
-l3.grid(padx=1, pady=0)
+    l3 = ttk.Label(self.root, text=self.title_list[2], font="helvetica 16", width=28)
+    l3.grid(padx=1, pady=0)
 
-# def font_changed(font):
+def main():
+  ds = DiskSpeed();
+  ds.root.mainloop()
+  
+if __name__ == '__main__':
+  main()
+
+
+      # def font_changed(font):
 #     l['font'] = font
 
 # root.tk.call('tk', 'fontchooser', 'configure', '-font', 'helvetica 24', '-command', root.register(font_changed))
 # root.tk.call('tk', 'fontchooser', 'show')
 
-root.mainloop()
+
