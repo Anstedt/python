60adc1453ab6d90b295e15ebcdf86a31a3e31c29
Anstedt <anstedt@gmail.com>
2024-03-06 12:39:53 -0600

    Cleaned up, less debug

--- diskspeed/diskspeed.py
+++ diskspeed/diskspeed.py
@@ -7,7 +7,7 @@ from tkinter import ttk
 
 class DiskSpeed:
   disks = []
-    
+
   def __init__(self):
     self.root = Tk()
     self.get_disks()
@@ -16,34 +16,20 @@ class DiskSpeed:
     self.display_disk_info()
 
   def my_print(self, event):
-    print(self.value_list[0])
-    # l1r.config(text = str(self.value_list[0]))
-    l1r_text.set(self.disks[self.disk_letters.current()])
+    # print(self.value_list[0])
+    l1r.config(text = str(self.value_list[0]))
+    # This works but not neede here: l1r_text.set(self.disks[self.disk_letters.current()])
     l2r.config(text = str(self.value_list[1]))
     l3r.config(text = str(self.value_list[2]))
 
     # Update Combo box
     self.combo_text.set(self.disks[self.disk_letters.current()])
-    
-    # global disk_ltr
-    # disk_ltr = tk.StringVar()
-    # disk_ltr.set(self.disks[self.disk_letters.current()])
-    # # entry = "Letter"+disk_ltr+"HOWARD"
-    # l1r_text.set(disk_ltr.get())
-    # print(l1r_text.get())
-    # l2r_text = "NEW"
-    # l3r_text = "OLD"
-    # print(disk_ltr)
-    # l1r.config(textvariable = l1r_text)
-    # l2r.config(textvariable = l2r_text)
-    # l3r.config(text = "WOW")
-    # print("You Select" , self.disks[self.disk_letters.current()], "This Letter", event)
-    
+
   def get_disks(self):
     print("Start Disk Letters")
     drives_cmd = "wmic logicaldisk get name > drive_letters.log"
     os.system(drives_cmd)
-    
+
     # Now read the file and process it
     file = open("drive_letters.log", 'rt')
     lines = file.read().splitlines()
@@ -53,9 +39,9 @@ class DiskSpeed:
     for line in lines:
       index = line.find(":")
       if (index != -1):
-        print("line=", line, "x=", x, "line02=", (line[1:2]))
+        # print("line=", line, "x=", x, "line02=", (line[1:2]))
         self.disks.insert(x, (line[1:2]))
-        print(self.disks[x])
+        # print(self.disks[x])
         x += 1
 
     #self.disk_name = tk.StringVar(value = self.disks[0])
@@ -72,7 +58,7 @@ class DiskSpeed:
     ### disk_letters.bind('<<ComboboxSelected>>', lambda even: print(self.disk_name.get()))
     self.disk_letters.bind('<<ComboboxSelected>>', self.my_print)
 
-    
+
   def get_disk_info(self):
     print("Start Disk Speed")
     # Disk Speed
@@ -81,8 +67,8 @@ class DiskSpeed:
     logf="disk_"+dsk_drv+"_info.log"
 
     # Get Disk Info
-    ### result = subprocess.run(["winsat" , "disk" , "-drive", "C"], shell=True, capture_output=True, text=True)
-    ### print("Result:", result.stdout, file=open(logf, 'w'))
+    result = subprocess.run(["winsat" , "disk" , "-drive", "C"], shell=True, capture_output=True, text=True)
+    print("Result:", result.stdout, file=open(logf, 'w'))
 
     # Now read the file and process it
     file = open(logf, 'rt')
@@ -113,7 +99,7 @@ class DiskSpeed:
         self.value_list[list_index] = float(result[:result.find(" ")])
         self.units_list[list_index] = result[result.find(" "):]
         list_index = list_index + 1
-        
+
   def display_disk_info(self):
     ttk.Entry(self.root).grid(column = 1, row = 0)
 
@@ -122,19 +108,26 @@ class DiskSpeed:
     lp.grid(padx=0, pady=0)
     lp.grid(column = 0, row = 1)
 
-    # Disk Info
+    # Disk Info 1
     l1 = ttk.Label(self.root, text=self.title_list[0], font="helvetica 16", width=28)
     l1.grid(padx=1, pady=0)
     l1.grid(column = 0, row = 2)
 
     global l1r_text, l1r
     l1r_text = StringVar()
-    l1r_text.set("TEST")
+    l1r_text.set(self.value_list[0])
     l1r = ttk.Label(self.root, textvariable=l1r_text, font="helvetica 16", width=28)
     l1r.grid(padx=1, pady=0)
     l1r.grid(column = 1, row = 2)
-    
-    # Disk Info
+
+    global l1v_text, l1v
+    l1v_text = StringVar()
+    l1v_text.set(self.units_list[0])
+    l1v = ttk.Label(self.root, textvariable=l1v_text, font="helvetica 16", width=28)
+    l1v.grid(padx=1, pady=0)
+    l1v.grid(column = 2, row = 2)
+
+    # Disk Info 2
     l2 = ttk.Label(self.root, text=self.title_list[1], font="helvetica 16", width=28)
     l2.grid(padx=1, pady=0)
     l2.grid(column = 0, row = 3)
@@ -144,7 +137,14 @@ class DiskSpeed:
     l2r.grid(padx=1, pady=0)
     l2r.grid(column = 1, row = 3)
 
-    # Disk Info
+    global l2v_text, l2v
+    l2v_text = StringVar()
+    l2v_text.set(self.units_list[1])
+    l2v = ttk.Label(self.root, textvariable=l2v_text, font="helvetica 16", width=28)
+    l2v.grid(padx=1, pady=0)
+    l2v.grid(column = 2, row = 3)
+
+    # Disk Info 3
     l3 = ttk.Label(self.root, text=self.title_list[2], font="helvetica 16", width=28)
     l3.grid(padx=1, pady=0)
     l3.grid(column = 0, row = 4)
@@ -153,6 +153,13 @@ class DiskSpeed:
     l3r = ttk.Label(self.root, text="three", font="helvetica 16", width=28)
     l3r.grid(padx=1, pady=0)
     l3r.grid(column = 1, row = 4)
+
+    global l3v_text, l3v
+    l3v_text = StringVar()
+    l3v_text.set(self.units_list[2])
+    l3v = ttk.Label(self.root, textvariable=l3v_text, font="helvetica 16", width=28)
+    l3v.grid(padx=1, pady=0)
+    l3v.grid(column = 2, row = 4)
 
 def main():
   ds = DiskSpeed();
